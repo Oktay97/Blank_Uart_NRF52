7e6575774159dd967d3eb1fbba1b466404912218
Oktay Topcu <oktaytopcu97@gmail.com>
2023-12-27 22:06:02 +0300

    first commit

---
+++ pca10056/s140/ses/Output/Release/Obj/ble_app_uart_pca10056_s140/system_nrf52840.d
@@ -0,0 +1,26 @@
+Output/Release/Obj/ble_app_uart_pca10056_s140/system_nrf52840.o: \
+ D\:\_FW\NRF\nRF5_SDK_17.1.0\modules\nrfx\mdk\system_nrf52840.c \
+ D\:\_FW\NRF\nRF5_SDK_17.1.0\modules\nrfx\mdk\system_nrf52.c \
+ D\:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.40/include/stdint.h \
+ D\:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.40/include/stdbool.h \
+ D\:\_FW\NRF\nRF5_SDK_17.1.0\modules\nrfx\mdk\nrf.h \
+ D\:\_FW\NRF\nRF5_SDK_17.1.0\modules\nrfx\mdk\nrf52840.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cm4.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_version.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_compiler.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../../../../../components/toolchain/cmsis/include/mpu_armv7.h \
+ D\:\_FW\NRF\nRF5_SDK_17.1.0\modules\nrfx\mdk\system_nrf52840.h \
+ D\:\_FW\NRF\nRF5_SDK_17.1.0\modules\nrfx\mdk\nrf52840_bitfields.h \
+ D\:\_FW\NRF\nRF5_SDK_17.1.0\modules\nrfx\mdk\nrf51_to_nrf52840.h \
+ D\:\_FW\NRF\nRF5_SDK_17.1.0\modules\nrfx\mdk\nrf52_to_nrf52840.h \
+ D\:\_FW\NRF\nRF5_SDK_17.1.0\modules\nrfx\mdk\compiler_abstraction.h \
+ D\:\_FW\NRF\nRF5_SDK_17.1.0\modules\nrfx\mdk\nrf_peripherals.h \
+ D\:\_FW\NRF\nRF5_SDK_17.1.0\modules\nrfx\mdk\nrf52840_peripherals.h \
+ D\:\_FW\NRF\nRF5_SDK_17.1.0\modules\nrfx\mdk\nrf_erratas.h \
+ D\:\_FW\NRF\nRF5_SDK_17.1.0\modules\nrfx\mdk\nrf51_erratas.h \
+ D\:\_FW\NRF\nRF5_SDK_17.1.0\modules\nrfx\mdk\nrf52_erratas.h \
+ D\:\_FW\NRF\nRF5_SDK_17.1.0\modules\nrfx\mdk\nrf53_erratas.h \
+ D\:\_FW\NRF\nRF5_SDK_17.1.0\modules\nrfx\mdk\nrf91_erratas.h \
+ D\:\_FW\NRF\nRF5_SDK_17.1.0\modules\nrfx\mdk\system_nrf52.h \
+ D\:\_FW\NRF\nRF5_SDK_17.1.0\modules\nrfx\mdk\system_nrf52_approtect.h
---
+++ pca10056/s140/ses/Output/Release/Obj/ble_app_uart_pca10056_s140/utf.d
@@ -0,0 +1,6 @@
+Output/Release/Obj/ble_app_uart_pca10056_s140/utf.o: \
+ D\:\_FW\NRF\nRF5_SDK_17.1.0\external\utf_converter\utf.c \
+ D\:\_FW\NRF\nRF5_SDK_17.1.0\external\utf_converter\utf.h \
+ D\:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.40/include/stddef.h \
+ D\:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.40/include/__crossworks.h \
+ D\:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.40/include/stdint.h
